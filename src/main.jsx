d5b87687bcb3b48210cc50526322f2b3bf49e713
Sascha Koeppe <saschakoeppe@gmail.com>
2023-10-12 09:45:17 +0900

    add greeting + some list

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Greeting from './Greeting.jsx'
+import ListBallInput from './ListBallInput.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Greeting />
+    <ListBallInput />
   </React.StrictMode>,
 )
